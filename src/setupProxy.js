6ec457064f760c5116bf64e96ef20aecbade4f1b
Martin <martin.gebov@gmail.com>
2023-05-26 21:53:15 +0300

    wip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
@@ -7,11 +7,34 @@ module.exports = function (app) {
             secure: false,
             target: '',
             changeOrigin: true,
+            selfHandleResponse: true,
             onProxyReq: (proxyReq, req, res) => {
                 proxyReq.setHeader('X-ORIGINAL-HOST', 'localhost:3000');
             },
-            onProxyRes: (proxyRes) => {
-            },
+            onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+                if (req.url.indexOf("pages/Default.GetPageTemplates") != -1 || req.url.indexOf("templates/Default.GetPageTemplates") != -1) {
+                    const response = responseBuffer.toString('utf8');
+                    let responseAsJson = JSON.parse(response);
+                    responseAsJson.value.splice(0, 0, {
+                        Subtitle: "New editor",
+                        Title: "Local React Templates",
+                        Type: 0,
+                        Visible: true,
+                        Templates: [{
+                            Framework: 1,
+                            Id: "00000000-0000-0000-0000-000000000000",
+                            Name: "React.Default",
+                            ThumbnailUrl : "",
+                            Title: "Default",
+                            UsedByNumberOfPages: 0
+                        }]
+                    });
+
+                    return JSON.stringify(responseAsJson);
+                }
+
+                return responseBuffer;
+            }),
             onError: (err, req, res) => {
             }
         })
